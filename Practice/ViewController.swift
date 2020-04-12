8519b71d5114921cacc08a96a63d98319b73a1fb
Siddhesh <siddhesh.j@infiny.in>
2020-04-12 20:28:12 +0530

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Siddhesh jadhav on 12/04/20.
+//  Copyright © 2020 infiny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
